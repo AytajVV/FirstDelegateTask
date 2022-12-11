28a760ae1082dddb6508581df5f9660a05e9fa66
Veyisli Aytaj <AytajVV@code.local>
2022-12-11 19:29:21 +0400

    1st predicate task

---
+++ FirstDelegateTask/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDelegateTask
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+
+        public Employee(string name, int age, int salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+        
+        public void AverageSalary(Employee item)
+        {
+
+           
+        }
+    }
+}
---
+++ FirstDelegateTask/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDelegateTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> list = new List<string>();
+            //list.Add("lorem");
+            //list.Add("ipsum");
+            //Console.WriteLine(list.Find(l=> l == "ayan"));
+            Employee person1 = new Employee("lorem", 25, 500);
+            Employee person2 = new Employee("lorem1", 20, 1000);
+            Employee person3 = new Employee("lorem2", 30, 1500);
+            Employee person4 = new Employee("lorem3", 45, 5000);
+
+            List<Employee> list = new List<Employee>();
+            list.Add(person1);
+            list.Add(person2);
+            list.Add(person3);
+            list.Add(person4);
+            int sum = 0;
+            int count = 0;
+
+            
+            foreach (var item in list.FindAll(employee=>employee.Age>20 && employee.Age<40))
+            {
+                
+                sum += item.Salary;
+                    count++;
+            }
+            int avg = sum / count;
+            Console.WriteLine(avg);
+
+
+
+        }
+        
+
+    }
+}
+
+
